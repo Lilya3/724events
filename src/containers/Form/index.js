d05b303fe509491a1d293f0883557ea515749c1d
Lilya3 <bazaralily4@gmail.com>
2026-03-02 01:32:28 +0100

    trigger onSuccess callback after successful form submission

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -13,9 +13,11 @@ const Form = ({ onSuccess, onError }) => {
       evt.preventDefault();
       setSending(true);
       // We try to call mockContactApi
+
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
